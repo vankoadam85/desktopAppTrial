da5d7b108239da9e58213cf006172b1a5fe5fcea
Vankó <vankoadam85@gmail.com>
2019-03-18 16:38:23 +0100

    Add DB management with EF

---
+++ partnertar/partnertarServer/Data/PartnertarContext.cs
@@ -0,0 +1,19 @@
+﻿using partnertarServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace partnertarServer.Data
+{
+    public class PartnertarContext : DbContext
+    {
+        public PartnertarContext() : base ("name=PartnertarDBConnectionString")
+        {
+            Database.SetInitializer<PartnertarContext>(new DropCreateDatabaseIfModelChanges<PartnertarContext>());
+        }
+
+        public DbSet<Partner> Partners { get; set; }
+    }
+}
